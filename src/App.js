1d76faf7bbfccfdfdbb370331ee623463c6c5bad
loci <lociworld@gmail.com>
2020-05-26 02:07:29 +0900

    #3.1 All you need to know about State- setState, current

--- src/App.js
+++ src/App.js
@@ -7,24 +7,23 @@ class App extends React.Component{
     count: 0
   };
   add = () => {
-    console.log("add");
-  };
-     
+    // this.setState({ count: this.state.count +1 }); //가능은 하지만 직접 state 수정하지 말 것
+    this.setState(current => ({ count: current.count +1 }));
+  };   
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count -1 }));
   };
  
 
-  render(){
-  return <div>
-    <h1>The number is: {this.state.count}</h1>
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button>
-
-    </div>
+  render() {
+      return (
+            <div>
+              <h1>The number is: {this.state.count}</h1>
+              <button onClick={this.add}>Add</button>
+              <button onClick={this.minus}>Minus</button>
+             </div>
+      );
   }
-
 }
 
 export default App;
-
