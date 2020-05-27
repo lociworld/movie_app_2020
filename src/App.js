5ab831f02a84f394dbf2335cdc5f3ad722196f2e
loci <lociworld@gmail.com>
2020-05-27 09:42:42 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 
 
@@ -7,10 +8,12 @@ class App extends React.Component{
     isLoading: true,
     movies: []
   }
+  
+  getMovies = async() => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getmovies();
   }
   render() {
   const { isLoading } = this.state;
