0ee87fef7ebdad8eff84eecbb03b65cf5ff80fe6
loci <lociworld@gmail.com>
2020-05-31 22:03:44 +0900

    #4.4 Styles Timelaps

--- src/Movie.js
+++ src/Movie.js
@@ -7,7 +7,7 @@ return <div className="movie">
     
     <img src={poster} alt={title} title={title} />
     <div className="movie__data">
-    <h3 className="movie__title" style={{ backgroundColor: "red"}}>{ title }</h3>
+    <h3 className="movie__title">{ title }</h3>
     <h5 className="movie__year">{ year }</h5>
     <p className="movie__summary">{summary}</p>
     <ul className="genres">
