987f1d3d1d8acbf65e7f76c7f3eec88b4f5fb1d4
loci <lociworld@gmail.com>
2020-06-01 20:53:24 +0900

    #5.0 Deploying to Github Pages

---
+++ build/precache-manifest.6bbb26efe13785174b9d1da5d3e973e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2aa1b20124d91fdd978c8ae6f1c8e9",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b206ed7379587266f665",
+    "url": "/movie_app_2020/static/css/main.2dedfbad.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b206ed7379587266f665",
+    "url": "/movie_app_2020/static/js/main.4a82b4b5.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
--- src/Movie.js
+++ src/Movie.js
@@ -9,12 +9,13 @@ return <div className="movie">
     <div className="movie__data">
     <h3 className="movie__title">{ title }</h3>
     <h5 className="movie__year">{ year }</h5>
-    <p className="movie__summary">{summary}</p>
-    <ul className="genres">
+    <ul className="movie__genres">
         {genres.map((genre,idx) => (
         <li key={idx} className="genres__genre">{genre}</li>
         ))}
     </ul>
+    <p className="movie__summary">{summary.slice(0,140)}...</p>
+   
     </div>
 </div>
 }
