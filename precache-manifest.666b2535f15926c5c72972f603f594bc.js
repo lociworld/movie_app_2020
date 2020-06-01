ce1390fb4321434195f71a0b5b91c9447ddc491f
loci <lociworld@gmail.com>
2020-06-01 20:40:33 +0900

    Updates

---
+++ precache-manifest.666b2535f15926c5c72972f603f594bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1e81cf438d03034ebd2a335b67e4ba",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "38befac06e138d2ff051",
+    "url": "/movie_app_2020/static/css/main.5e9b72c7.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38befac06e138d2ff051",
+    "url": "/movie_app_2020/static/js/main.4a82b4b5.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
