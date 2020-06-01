267cf6fe4ad1ea54ec8ede755c61b522193fb485
loci <lociworld@gmail.com>
2020-06-02 00:27:04 +0900

    #6.1 Building the Router

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 import "./Home.css";
 
 
@@ -54,4 +54,4 @@ class Home extends React.Component{
    }
   }
 
-export default App;
+export default Home;
