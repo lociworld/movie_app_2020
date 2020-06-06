e57c99828b1921f100db39a6400f747f7737e482
loci <lociworld@gmail.com>
2020-06-06 22:38:35 +0900

    #6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { HashRouter, Route }  from "react-router-dom"
 import Home from "./routes/Home"
 import About from "./routes/About"
-import Navigation from "./components/Navigation";
-import "./App.css"
-
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation.js";
+import "./App.css";
 
 function App() {
   return ( 
@@ -12,6 +12,7 @@ function App() {
     <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/movie-detail" component={Detail} />
   </HashRouter>
   );
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,16 @@
 import React from "react";
+import "./About.css";
 
 
-function About() {
-    return <span>About this page: I built it because I love movies.</span>
+function About(props) {
+    console.log(props); 
+    
+    return (
+        <div className="about_container">
+          <span>About this page: I built it because I love movies.</span>
+        </div>
+    );
+   
 }
 
 export default About;
